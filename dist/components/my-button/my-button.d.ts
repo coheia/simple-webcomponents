9707f86326a625ffd924426411aa18d795fc90f8
Felipe Correia <felipe.souza@redeancora.com.br>
2023-02-13 11:39:53 -0300

    deletando dist do versionamento

--- dist/components/my-button/my-button.d.ts
+++
@@ -1,5 +0,0 @@
-import { LitElement } from 'lit-element';
-export declare class MyButton extends LitElement {
-    static get styles(): import("lit-element").CSSResult;
-    render(): import("lit-html").TemplateResult<1>;
-}
