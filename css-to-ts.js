b363670c8d571e8d72ad79ce547bd0ba0f924b69
Felipe Correia <felipe.souza@redeancora.com.br>
2023-02-13 21:07:19 -0300

    modificando css-to-ts file to remove coments from generated files

--- css-to-ts.js
+++ css-to-ts.js
@@ -10,14 +10,10 @@ for (let i = 2; i < process.argv.length; i++) {
   try {
     const filePath = process.argv[i];
     const content = fs.readFileSync(filePath);
-    fs.writeFileSync(`${filePath}.ts`, `/**
-  * @license
-  * Copyright 2022 Google LLC
-  * SPDX-License-Identifier: Apache-2.0
-  */
- import {css} from 'lit';
- export const styles = css\`${content.toString('utf8')}\`;
- `);
+    fs.writeFileSync(`${filePath}.ts`, `
+      import {css} from 'lit';
+      export const styles = css\`${content.toString('utf8')}\`;
+    `);
 
   } catch (error) {
     console.error(error);
