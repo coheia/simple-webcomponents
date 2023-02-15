c69d7383f572ba74692dbea79bebfafe61e26ace
Felipe Correia <felipe.souza@redeancora.com.br>
2023-02-15 18:46:13 -0300

    sb7 + vite partial

---
+++ .storybook/main.ts
@@ -0,0 +1,29 @@
+import type { StorybookConfig } from '@storybook/web-components-vite';
+import { mergeConfig } from 'vite';
+const config: StorybookConfig = {
+  "stories": [
+    "../src/**/*.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials"
+  ],
+  "framework": {
+    "name": "@storybook/web-components-vite",
+    "options": {}
+  },
+  "docs": {
+    "autodocs": "tag"
+  },
+  async viteFinal(config) {
+   // Merge custom configuration into the default config
+   return mergeConfig(config, {
+     // Add storybook-specific dependencies to pre-optimization
+     optimizeDeps: {
+       include: ['storybook-addon-designs'],
+     },
+   });
+ },
+};
+export default config;
---
+++ src/stories/Page.stories.js
@@ -0,0 +1,14 @@
+import { Page } from './Page';
+
+export default {
+  title: 'Example/Page',
+  render: (args) => Page(args),
+};
+
+export const LoggedIn = {
+  args: {},
+};
+
+export const LoggedOut = {
+  args: {},
+};
